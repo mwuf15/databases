25187f7302b80be82a464abcaa6c4119a13d46e1
Anqi Lu <anqi0103@gmail.com>
2020-07-22 21:41:56 -0700

    Finish the schema.sql file

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,31 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  roomName VARCHAR(20) NOT NULL,
+  messages VARCHAR(200) NOT NULL,
+  user INTEGER NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+DROP TABLE IF EXISTS username;
 
+CREATE TABLE username (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  names VARCHAR(20) NOT NULL,
+  Age INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+ALTER TABLE messages ADD FOREIGN KEY (user) REFERENCES username (id);
 
 
 /*  Execute this file from the command line by typing:
